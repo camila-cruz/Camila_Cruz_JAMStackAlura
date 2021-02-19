75b36e99b67cf3709692ccf1773f782e9fb6362a
Camila <cruz.camila001@gmail.com>
2021-02-18 21:03:40 -0300

    Cria componente Box

---
+++ src/components/foundation/layout/Box/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Box = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: space-between; 
+`
