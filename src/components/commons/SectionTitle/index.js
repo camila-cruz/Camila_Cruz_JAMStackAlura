25ead15ede864276ce82c4743026c56d6f12b3d8
Camila <cruz.camila001@gmail.com>
2021-02-20 16:30:42 -0300

    Ajusta o espaçamento e estilo da SectionTitle

--- src/components/commons/SectionTitle/index.js
+++ src/components/commons/SectionTitle/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
 
 export const SectionTitle = styled.h2`
-  font-size: ${({ theme }) => theme.typography.h2.size[0]}px;
+  font-size: ${({ theme }) => theme.typography.h2.size[2]}px;
   font-weight: ${({ theme }) => theme.weight.bold};
+
+  color: ${({ theme }) => theme.mainUi.text.light.primary};
+  margin-bottom: ${({ theme }) => theme.spacing[1]}px;
+
+  ${breakpointsMedia({
+    md: css`
+      font-size: ${({ theme }) => theme.typography.h2.size[0]}px;
+      margin-bottom: ${({ theme }) => theme.spacing[2]}px;
+    `
+  })}
 
 `
