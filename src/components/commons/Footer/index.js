585458fb10ef599d0acf1e078afe09ed5608be2c
Camila <cruz.camila001@gmail.com>
2021-02-20 14:29:55 -0300

    Adiciona ícone e estilo ao Footer

--- src/components/commons/Footer/index.js
+++ src/components/commons/Footer/index.js
@@ -4,12 +4,23 @@ import styled from 'styled-components';
 const FooterBase = styled.footer`
   display: flex;
   justify-content: center;
+  align-items: center;
+  height: 70px;
+
+  background-color: ${({ theme }) => theme.mainUi.background.light.tertiary};
+`
+
+const FooterIcon = styled.img`
+  height: 32px;
+  color: ${({ theme }) => theme.mainUi.text.light.tertiary};
+  /* Yellow icon */
+  filter: invert(79%) sepia(60%) saturate(338%) hue-rotate(350deg) brightness(98%) contrast(94%);
 `
 
 export default function Footer() {
   return (
     <FooterBase>
-      [Github]
+      <FooterIcon src="/images/github.svg" alt="Ícone do GitHub" />
     </FooterBase>
   )
 }
