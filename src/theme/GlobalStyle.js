32458b92630b567075aeb6646decbe23c13404fa
Camila <cruz.camila001@gmail.com>
2021-02-20 10:18:04 -0300

    Corrige reset do GlobalStyle

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -2,15 +2,14 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   * {
+    margin: 0;
+    padding: 0;
     box-sizing: border-box;
     font-family: 'Nunito';
   }
   
   html,
   body {
-    margin: 0;
-    padding: 0;
-
     display: flex;
     min-height: 100vh;
     width: 100%;
