ea157ee2d816fb2a00f392f79b18126f98f49e27
Camila <cruz.camila001@gmail.com>
2021-02-21 13:49:43 -0300

    Adiciona variantes ao componente Text

--- src/components/foundation/Text/index.js
+++ src/components/foundation/Text/index.js
@@ -1,10 +1,49 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export default function Text({ children }) {
+import PropTypes from 'prop-types';
+import { propToStyle } from '../../../theme/utils/propToStyle';
+
+export const TextStyleVariantsMap = {
+  paragraph: css`
+    font-size: ${({ theme }) => theme.typography.paragraph.size[0]}px;
+    font-weight: ${({ theme }) => theme.weight.light};
+    line-height: ${({ theme }) => theme.typography.paragraph.lineHeight};
+  `,
+  link: css`
+    font-size: ${({ theme }) => theme.typography.paragraph.size[1]}px;
+    font-weight: ${({ theme }) => theme.weight.bold};
+  `,
+  strong: css`
+    font-weight: ${({ theme }) => theme.weight.bold};
+  `
+};
+
+const TextBase = styled.span`
+  ${(props) => TextStyleVariantsMap[props.variant]}
+  ${propToStyle('textAlign')}
+`;
+
+export default function Text({ tag, variant, children, ...props }) {
   return (
-    <p>
+    <TextBase
+      as={tag}
+      variant={variant}
+      {...props}
+    >
       {children}
-    </p>
-  )
+    </TextBase>
+  );
+}
+
+/* Validação dos tipos de entrada dos atributos */
+Text.propTypes = {
+  tag: PropTypes.string,
+  variant: PropTypes.string,
+  children: PropTypes.node.isRequired,
+}
+
+Text.defaultProps = {
+  tag: 'span',
+  variant: 'paragraph',
 }
