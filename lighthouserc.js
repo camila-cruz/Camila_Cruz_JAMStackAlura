f79ceb88361913fe088d8c7e64ae746411955ddf
Camila <cruz.camila001@gmail.com>
2021-02-28 18:10:32 -0300

    ci(performance): adiciona comando para quando o server está pronto no lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,12 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:3000/'],
       startServerCommand: 'npm run start',
+      startServerReadyPattern: 'ready on',
+      url: [
+        'http://localhost:3000/',
+      ],
+      numberOfRuns: 3,
     },
     upload: {
       target: 'temporary-public-storage',
