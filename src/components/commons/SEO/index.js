a5072d3bb1a1cbb2ae42fc1fee39dd9a54d9895a
Camila <cruz.camila001@gmail.com>
2021-03-19 22:34:53 -0300

    feat: adiciona meta description

--- src/components/commons/SEO/index.js
+++ src/components/commons/SEO/index.js
@@ -6,7 +6,7 @@ export default function SEO({ headTitle }) {
   const hasHeadTitle = Boolean(headTitle);
   const baseTitle = 'Camila Cruz - Portfólio';
   const title = hasHeadTitle ? `${headTitle} | ${baseTitle}` : baseTitle;
-  const description = '';
+  const description = 'Portfólio e blog da desenvolvedora brasileira Camila Cruz';
   const imageSrc = 'images/logo.png';
   const urlBase = 'https://portfolio-alura-camila-cruz.vercel.app';
   const image = `${urlBase}/${imageSrc}`;
