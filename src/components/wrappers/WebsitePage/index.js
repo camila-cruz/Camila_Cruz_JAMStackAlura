f0e1602ea6e318d1db8313704a35f119bd2751ea
Camila <cruz.camila001@gmail.com>
2021-03-21 15:26:32 -0300

    feat: adiciona context para a Modal de FormContato

--- src/components/wrappers/WebsitePage/index.js
+++ src/components/wrappers/WebsitePage/index.js
@@ -1,21 +1,43 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '../../foundation/layout/Box';
 import Cabecalho from '../../commons/Cabecalho';
 import Footer from '../../commons/Footer';
 import SEO from '../../commons/SEO';
+import Modal from '../../commons/Modal';
+import FormContato from '../../patterns/FormContato';
 
-export default function WebsitePageWrapper({ children, seoProps, menuProps }) {
+export const WebsitePageContext = createContext({
+  toggleModal: () => {},
+});
+
+export default function WebsitePageWrapper({
+  children,
+  seoProps,
+  menuProps,
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <>
+    <WebsitePageContext.Provider
+      value={{
+        toggleModal: () => {
+          setIsOpen(!isOpen);
+        },
+      }}
+    >
       <SEO {...seoProps} />
       <Box>
+        <Modal isOpen={isOpen} onClose={() => setIsOpen(!isOpen)}>
+          {(props) => <FormContato props={props} />}
+        </Modal>
+
         {menuProps.display && <Cabecalho />}
         {children}
         <Footer />
       </Box>
-    </>
+    </WebsitePageContext.Provider>
   );
 }
 
