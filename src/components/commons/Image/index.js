6ae5e1b7aa93c9414c55cfd074a69a20fbae6755
Camila <cruz.camila001@gmail.com>
2021-03-31 10:45:46 -0300

    refactor: adiciona box-shadow às imagens

--- src/components/commons/Image/index.js
+++ src/components/commons/Image/index.js
@@ -4,9 +4,10 @@ import { propToStyle } from '../../../theme/utils/propToStyle';
 export const Image = styled.img`
   width: auto;
 
-  ${({ bordered }) => bordered && css`
-    border: 1px solid ${({ theme }) => theme.mainUi.background.light.tertiary};
+  ${({ shadowed }) => shadowed && css`
+    /* border: 1px solid ${({ theme }) => theme.mainUi.background.light.tertiary}; */
     border-radius: 8px;
+    box-shadow: 0 4px 8px 0 #5129199e, 0 6px 20px 0 #51291952;
   `}
 
   ${propToStyle('height')};
