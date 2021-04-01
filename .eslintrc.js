505f6eee80c37a4685566f25b2f38de1328b3ce9
Camila <cruz.camila001@gmail.com>
2021-04-01 20:01:42 -0300

    build: instala e configura cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,10 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'cypress/global': true,
   },
   extends: [
+    'plugin:cypress/recommended',
     'plugin:react/recommended',
     'airbnb',
   ],
