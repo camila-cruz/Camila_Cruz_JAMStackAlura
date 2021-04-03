b20dbd00b91bc4d926b82f44c0d6a8438a93aeed
Camila <cruz.camila001@gmail.com>
2021-04-03 14:52:56 -0300

    test: serviço de contato

---
+++ src/services/HttpClient/index.js
@@ -0,0 +1,11 @@
+export async function HttpClient(url, { headers, body, ...options }) {
+  return fetch(url, {
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+    ...options,
+  })
+    .then((res) => res.json());
+}
