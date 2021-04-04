d458db94d0c637f1d9cbe9c7e7a966508e2698d8
Camila <cruz.camila001@gmail.com>
2021-04-04 20:22:24 -0300

    refactor: passa o tema dinamicamente pelas preferências do usuário

--- src/components/wrappers/WebsitePage/provider/index.js
+++ src/components/wrappers/WebsitePage/provider/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
-import theme from '../../../../theme';
+import useDarkMode from 'use-dark-mode';
+import { lightTheme, darkTheme } from '../../../../theme';
 import { GlobalStyle } from '../../../../theme/GlobalStyle';
 
 export default function WebsiteGlobalProvider({ children }) {
+  const { value } = useDarkMode(false, { storageKey: null, onChange: null });
+  const theme = value ? darkTheme : lightTheme;
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
